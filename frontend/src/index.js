5793c040cfecd66bb650e9058e254c046ae03eba
zedpro1230 <73679480+zedpro1230@users.noreply.github.com>
2025-11-06 17:53:46 +0700

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+
 import "./output.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "../src/redux/store";
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "475px",
+      },
+    },
   },
   plugins: [],
+  corePlugins: {
+    // Ensure all core plugins are enabled
+    preflight: true,
+  },
 };
